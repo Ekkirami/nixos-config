cf2b0166f2bf3e9cfba8603818b9faf2ee9500a1
Ekkirami <ekkirami@gmail.com>
2026-04-01 01:34:39 +0800

    change

--- configuration.nix
+++ configuration.nix
@@ -82,6 +82,8 @@
   ];
   environment.variables.EDITOR = "nvim";
 
+  programs.zsh.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
