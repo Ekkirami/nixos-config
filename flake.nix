cf6dfaffea9d455bf128972a52e93c305f711208
Ekkirami <ekkirami@gmail.com>
2026-04-01 01:42:43 +0800

    change

--- flake.nix
+++ flake.nix
@@ -5,10 +5,6 @@
     nixpkgs.url = "git+https://mirrors.nju.edu.cn/git/nixpkgs.git?ref=nixos-25.11&shallow=1";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -18,7 +14,6 @@
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        # home-manager
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
--- home.nix
+++ home.nix
@@ -1,64 +1,40 @@
 { config, pkgs, ... }:
 
 {
-  # 注意修改这里的用户名与用户目录
   home.username = "ekkirami";
   home.homeDirectory = "/home/ekkirami";
 
-  # 将当前配置目录中的文件导入 Nix store，并在 Home 目录下生成指向该 store 文件的符号链接
-  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
-
-  # 将 scripts 目录导入 Nix store，并在 Home 目录下递归生成指向 store 中的文件的符号链接
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   recursive = true;   # 递归整个文件夹
-  #   executable = true;  # 将其中所有文件添加「执行」权限
-  # };
-
-  # 直接以 text 的方式，在 nix 配置文件中硬编码文件内容
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
-
-  # 设置鼠标指针大小以及字体 DPI（适用于 4K 显示器）
-  # xresources.properties = {
-  #   "Xcursor.size" = 16;
-  #   "Xft.dpi" = 172;
-  # };
-
-  # 通过 home.packages 安装一些常用的软件
-  # 这些软件将仅在当前用户下可用，不会影响系统级别的配置
-  # 建议将所有 GUI 软件，以及与 OS 关系不大的 CLI 软件，都通过 home.packages 安装
-  home.packages = with pkgs;[
+  home.packages = with pkgs; [
+    # 系统信息和终端
     neofetch
-    nnn # terminal file manager
+    nnn
     alacritty
 
-    # archives
+    # 压缩工具
     zip
     xz
     unzip
     p7zip
 
-    # utils
-    ripgrep # recursively searches directories for a regex pattern
-    jq # A lightweight and flexible command-line JSON processor
-    yq-go # yaml processor https://github.com/mikefarah/yq
-    eza # A modern replacement for ‘ls’
-    fzf # A command-line fuzzy finder
+    # 实用工具
+    ripgrep
+    jq
+    yq-go
+    eza
+    fzf
     python3
 
-    # networking tools
-    mtr # A network diagnostic tool
+    # 网络工具
+    mtr
     iperf3
-    dnsutils  # `dig` + `nslookup`
-    ldns # replacement of `dig`, it provide the command `drill`
-    aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    socat # replacement of openbsd-netcat
-    nmap # A utility for network discovery and security auditing
-    ipcalc  # it is a calculator for the IPv4/v6 addresses
+    dnsutils
+    ldns
+    aria2
+    socat
+    nmap
+    ipcalc
 
-    # misc
+    # 杂项工具
     cowsay
     file
     which
@@ -69,43 +45,39 @@
     zstd
     gnupg
 
-    # nix related
-    # it provides the command `nom` works just like `nix`
-    # with more details log output
+    # Nix 相关
     nix-output-monitor
 
-    # productivity
-    hugo # static site generator
-    glow # markdown previewer in terminal
+    # 生产力工具
+    hugo
+    glow
 
-    btop  # replacement of htop/nmon
-    iotop # io monitoring
-    iftop # network monitoring
+    # 系统监控
+    btop
+    iotop
+    iftop
 
-    # system call monitoring
-    strace # system call monitoring
-    ltrace # library call monitoring
-    lsof # list open files
+    # 系统调用追踪
+    strace
+    ltrace
+    lsof
 
-    # system tools
+    # 系统工具
     sysstat
-    lm_sensors # for `sensors` command
+    lm_sensors
     ethtool
-    pciutils # lspci
-    usbutils # lsusb
+    pciutils
+    usbutils
   ];
 
-  # git 相关配置
   programs.git = {
     enable = true;
     userName = "Ekkirami";
     userEmail = "ekkirami@gmail.com";
   };
 
-  # 启用 starship，这是一个漂亮的 shell 提示符
   programs.starship = {
     enable = true;
-    # 自定义配置
     settings = {
       add_newline = false;
       aws.disabled = true;
@@ -114,10 +86,8 @@
     };
   };
 
-  # alacritty - 一个跨平台终端，带 GPU 加速功能
   programs.alacritty = {
     enable = true;
-    # 自定义配置
     settings = {
       env.TERM = "xterm-256color";
       font = {
@@ -132,15 +102,12 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    # zshrc
     initExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
       zstyle :compinstall filename '/home/ekkirami/.zshrc'
       autoload -Uz compinit
       compinit
     '';
-
-    # 别名
     shellAliases = {
       vim = "nvim";
       urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
@@ -148,13 +115,5 @@
     };
   };
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "25.11";
 }
